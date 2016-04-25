19d08cbe21b2e7f2a8a278ab6e6069c8c068b347
Mike Obremski <mygenesisunlimited@gmail.com>
2016-04-25 12:20:34 -0700

    Update responsive.js

--- responsive.js
+++ responsive.js
@@ -21,7 +21,7 @@ function adjustIframes()
 }
 $(window).on('resize load',adjustIframes);
 
-// Here's some css and html that doea the trick
+// Here's some css and html that does the trick
 
 <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'>
 <!!!!place iframe code here!!!!>
